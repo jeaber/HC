9b33fbdd05358f71f3b012547696988230049dc1
jeaber <jeaber@gmail.com>
2015-06-21 09:53:33 -0500

    cleanup

--- src_temp/showcase/controllers/home-ctrl.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jbz on 6/12/2015.
- */
-(function () {
-    angular.module('hintchirps')
-        .controller('HomeController', ['$scope', 'loggedIn', function ($scope, $state) {
-            //$state.transitionTo('home.default');
-        }]);
-})()
--- src_temp/src/app/showcase/controllers/home-ctrl.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by jbz on 6/12/2015.
- */
-angular.module('bookbottles-showcase')
-    .controller('HomeController',['$scope', 'loggedIn', function ($scope, loggedIn) {
-        $scope.loggedIn = loggedIn;
-    }]);
